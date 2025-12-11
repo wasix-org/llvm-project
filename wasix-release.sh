aad7ed7153f37cda3d1fa38103b8b2cdb1a0e03a
Zebreus <zebreus@zebre.us>
2025-12-11 22:27:02 +0100

    Attempt to fix windows build

--- wasix-release.sh
+++ wasix-release.sh
@@ -4,7 +4,16 @@ set -euxo pipefail
 
 OUTFILE=${OUTFILE:-LLVM-$(uname -s)-$(uname -m).tar.gz}
 
-cmake --build build -t bin/lld -t bin/clang -t bin/llvm-ar -t bin/llvm-nm -t bin/llvm-ranlib -j16
+# Set the suffix to .exe on windows
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+case "$OS" in linux*) OS="linux";; darwin*) OS="apple";; windows*|mingw*|msys*|cygwin*) OS="windows";; *) echo "Unsupported OS: $OS"; exit 1;; esac
+SUFFIX=""
+if test "$OS" = "windows"; then
+  SUFFIX=".exe"
+fi
+
+cmake --build build -t bin/lld$SUFFIX -t bin/clang$SUFFIX -t bin/llvm-ar$SUFFIX -t bin/llvm-nm$SUFFIX -t bin/llvm-ranlib$SUFFIX -j16
+ls build/bin
 
 # Bit of a weird name, but build* is already gitignored, so we abuse that.
 rm -rf build-release
@@ -16,10 +25,12 @@ cp -r build/bin build-release
 cp -r build/lib/clang build-release/lib
 
 # Remove unneccessary files
-rm -rf build-release/bin/*tblgen
-rm -rf build-release/bin/llvm-lit
+rm -rf build-release/bin/*tblgen*
+rm -rf build-release/bin/llvm-lit*
 # Strip binaries
 strip build-release/bin/*
+
+ls build-release/bin
 
 cd build-release
 tar -czf $OUTFILE bin lib
